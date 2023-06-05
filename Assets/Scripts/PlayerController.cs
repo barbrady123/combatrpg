a21f12ebcb36c8d318266b6d087d3438552e5f40
David White <dwhite@us.oneilsoft.com>
2023-06-05 05:05:21 -0700

    thru 12

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,65 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(SpriteRenderer))]
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private float _moveSpeed = 1f;
+
+    private PlayerControls _playerControls;
+
+    private Vector2 _movement;
+
+    private Rigidbody2D _rb;
+
+    private Animator _animator;
+
+    private SpriteRenderer _spriteRenderer;
+
+    private void Awake()
     {
-        
+        _playerControls = new PlayerControls();
+        _rb = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        PlayerInput();
+    }
+
+    private void FixedUpdate()
+    {
+        AdjustPlayerFacingDirection();
+        Move();
+    }
+
+    private void OnEnable()
+    {
+        _playerControls.Enable();
+    }
+
+    private void PlayerInput()
+    {
+        _movement = _playerControls.Movement.Move.ReadValue<Vector2>();
+
+        _animator.SetFloat("moveX", _movement.x);
+        _animator.SetFloat("moveY", _movement.y);
+    }
+
+    private void Move()
+    {
+        _rb.MovePosition(_rb.position + (_movement * (_moveSpeed * Time.fixedDeltaTime)));
+    }
+
+    private void AdjustPlayerFacingDirection()
+    {
+        var mousePos = Input.mousePosition;
+        var playerScreenPoint = Camera.main.WorldToScreenPoint(transform.position);
+
+        _spriteRenderer.flipX = Input.mousePosition.x < playerScreenPoint.x;
     }
 }
